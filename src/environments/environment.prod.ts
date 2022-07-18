5bd1acbe1b5fc0e736d35823da05f4668b50f15b
Sheshanath <35036388+sheshanathkumar@users.noreply.github.com>
2022-07-18 12:42:54 +0530

    change environment value

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //api_url: 'http://cicdapp-env.eba-3qw9smzs.ap-south-1.elasticbeanstalk.com',
-  api_url : 'http://localhost:5000',
+  api_url: 'http://cicd.ap-south-1.elasticbeanstalk.com/',
+  //api_url : 'http://localhost:5000',
   production: true
 };
